4ddf845bc48d2bdef8a8ee53e0f6c4e4a9bee105
louyun <louyun@fuebiosciences.com>
2025-03-24 14:07:14 +0800

    previous miss commit

--- viputool/ut_robot_wrapper/camera_calibration/cameracalib_config.h
+++ viputool/ut_robot_wrapper/camera_calibration/cameracalib_config.h
@@ -9,12 +9,13 @@ namespace CameraCalib_Config
     /* File Path relative to executabale path */
     namespace FileRelativePath
     {
-        extern const char CONFIG_GLOBAL_CAMERA_right_arm_joint_pose[];
         extern const char CONFIG_GLOBAL_CAMERA_right_leftEye_arm_joint_pose[];
         extern const char CONFIG_GLOBAL_CAMERA_right_rightEye_arm_joint_pose[];
         extern const char CONFIG_GLOBAL_CAMERA_right_duelEye_arm_joint_pose[];
         extern const char CONFIG_FOLLOWING_CAMERA_right_arm_joint_pose[];
-        extern const char CONFIG_LOCAL_CAMERA_left_arm_joint_pose[];
+        extern const char CONFIG_LOCAL_CAMERA_left_leftEye_arm_joint_pose[];
+        extern const char CONFIG_LOCAL_CAMERA_left_rightEye_arm_joint_pose[];
+        extern const char CONFIG_LOCAL_CAMERA_left_duelEye_arm_joint_pose[];
     }
 }
 #endif // CAMERACALIB_CONFIG_H
--- viputool/ut_robot_wrapper/camera_calibration/cameracalibqwrapper.h
+++ viputool/ut_robot_wrapper/camera_calibration/cameracalibqwrapper.h
@@ -18,10 +18,13 @@ public:
     enum CameraCalib_Type{
         // GLOBAL_CAMERA_RIGHT_ARM         = 1,
         FOLLOWING_CAMERA_RIGHT_ARM      = 2,
-        LOCAL_CAMERA_LEFT_ARM           = 3,
+        // LOCAL_CAMERA_LEFT_ARM           = 3,
         GLOBAL_CAMERA_RIGHT_ARM_LEFT    = 4,
         GLOBAL_CAMERA_RIGHT_ARM_RIGHT   = 5,
         GLOBAL_CAMERA_RIGHT_ARM_DUEL    = 6,
+        LOCAL_CAMERA_LEFT_ARM_LEFT      = 7,
+        LOCAL_CAMERA_LEFT_ARM_RIGHT     = 8,
+        LOCAL_CAMERA_LEFT_ARM_DUEL      = 9,
     };
 
     explicit CameraCalibQWrapper(QObject *parent = nullptr);
@@ -34,7 +37,10 @@ public:
     Q_INVOKABLE bool updata_Joint_Pos_Global_Right_DuelEye(QString source_file_path);
 
     Q_INVOKABLE bool updata_Joint_Pos_Following_Right(QString source_file_path);
-    Q_INVOKABLE bool updata_Joint_Pos_Local_Left(QString source_file_path);
+
+    Q_INVOKABLE bool updata_Joint_Pos_Local_Left_LeftEye(QString source_file_path);
+    Q_INVOKABLE bool updata_Joint_Pos_Local_Left_RightEye(QString source_file_path);
+    Q_INVOKABLE bool updata_Joint_Pos_Local_Left_DuelEye(QString source_file_path);
 
     Q_PROPERTY( int joint_pos_index_global_right_LeftEye READ joint_pos_index_global_right_LeftEye WRITE set_joint_pos_index_global_right_LeftEye NOTIFY joint_pos_index_global_right_LeftEye_Changed FINAL)
     Q_PROPERTY( int joint_pos_total_num_global_right_LeftEye READ joint_pos_total_num_global_right_LeftEye WRITE set_joint_pos_total_num_global_right_LeftEye NOTIFY joint_pos_total_num_global_right_LeftEye_Changed FINAL)
@@ -52,9 +58,18 @@ public:
     Q_PROPERTY( int joint_pos_total_num_following_right READ joint_pos_total_num_following_right WRITE set_joint_pos_total_num_following_right NOTIFY joint_pos_total_num_following_right_Changed FINAL)
     Q_PROPERTY( QVariantList current_joint_pos_following_right READ get_current_joint_pos_following_right WRITE set_current_joint_pos_following_right NOTIFY current_joint_pos_following_right_changed)
 
-    Q_PROPERTY( int joint_pos_index_local_left READ joint_pos_index_local_left WRITE set_joint_pos_index_local_left NOTIFY joint_pos_index_local_left_Changed FINAL)
-    Q_PROPERTY( int joint_pos_total_num_local_left READ joint_pos_total_num_local_left WRITE set_joint_pos_total_num_local_left NOTIFY joint_pos_total_num_local_left_Changed FINAL)
-    Q_PROPERTY( QVariantList current_joint_pos_local_left READ get_current_joint_pos_local_left WRITE set_current_joint_pos_local_left NOTIFY current_joint_pos_local_left_changed)
+    Q_PROPERTY( int joint_pos_index_local_left_LeftEye READ joint_pos_index_local_left_LeftEye WRITE set_joint_pos_index_local_left_LeftEye NOTIFY joint_pos_index_local_left_LeftEye_Changed FINAL)
+    Q_PROPERTY( int joint_pos_total_num_local_left_LeftEye READ joint_pos_total_num_local_left_LeftEye WRITE set_joint_pos_total_num_local_left_LeftEye NOTIFY joint_pos_total_num_local_left_LeftEye_Changed FINAL)
+    Q_PROPERTY( QVariantList current_joint_pos_local_left_LeftEye READ get_current_joint_pos_local_left_LeftEye WRITE set_current_joint_pos_local_left_LeftEye NOTIFY current_joint_pos_local_left_LeftEye_changed)
+
+    Q_PROPERTY( int joint_pos_index_local_left_RightEye READ joint_pos_index_local_left_RightEye WRITE set_joint_pos_index_local_left_RightEye NOTIFY joint_pos_index_local_left_RightEye_Changed FINAL)
+    Q_PROPERTY( int joint_pos_total_num_local_left_RightEye READ joint_pos_total_num_local_left_RightEye WRITE set_joint_pos_total_num_local_left_RightEye NOTIFY joint_pos_total_num_local_left_RightEye_Changed FINAL)
+    Q_PROPERTY( QVariantList current_joint_pos_local_left_RightEye READ get_current_joint_pos_local_left_RightEye WRITE set_current_joint_pos_local_left_RightEye NOTIFY current_joint_pos_local_left_RightEye_changed)
+
+    Q_PROPERTY( int joint_pos_index_local_left_DuelEye READ joint_pos_index_local_left_DuelEye WRITE set_joint_pos_index_local_left_DuelEye NOTIFY joint_pos_index_local_left_DuelEye_Changed FINAL)
+    Q_PROPERTY( int joint_pos_total_num_local_left_DuelEye READ joint_pos_total_num_local_left_DuelEye WRITE set_joint_pos_total_num_local_left_DuelEye NOTIFY joint_pos_total_num_local_left_DuelEye_Changed FINAL)
+    Q_PROPERTY( QVariantList current_joint_pos_local_left_DuelEye READ get_current_joint_pos_local_left_DuelEye WRITE set_current_joint_pos_local_left_DuelEye NOTIFY current_joint_pos_local_left_DuelEye_changed)
+
 
     int joint_pos_index_following_right() const;
     void set_joint_pos_index_following_right(int newJoint_pos_index_following_right);
@@ -64,15 +79,6 @@ public:
 
     QVariantList get_current_joint_pos_following_right() const;
     void set_current_joint_pos_following_right(const QVariantList &newCurrent_joint_pos_following_right);
-
-    int joint_pos_index_local_left() const;
-    void set_joint_pos_index_local_left(int newJoint_pos_index_local_left);
-
-    int joint_pos_total_num_local_left() const;
-    void set_joint_pos_total_num_local_left(int newJoint_pos_total_num_local_left);
-
-    QVariantList get_current_joint_pos_local_left() const;
-    void set_current_joint_pos_local_left(const QVariantList &newCurrent_joint_pos_local_left);
 
     int joint_pos_index_global_right_LeftEye() const;
     void set_joint_pos_index_global_right_LeftEye(int newJoint_pos_index_global_right_LeftEye);
@@ -101,18 +107,39 @@ public:
     QVariantList get_current_joint_pos_global_right_DuelEye() const;
     void set_current_joint_pos_global_right_DuelEye(const QVariantList &newCurrent_joint_pos_global_right_DuelEye);
 
+    int joint_pos_index_local_left_LeftEye() const;
+    void set_joint_pos_index_local_left_LeftEye(int newJoint_pos_index_local_left_LeftEye);
+
+    int joint_pos_total_num_local_left_LeftEye() const;
+    void set_joint_pos_total_num_local_left_LeftEye(int newJoint_pos_total_num_local_left_LeftEye);
+
+    QVariantList get_current_joint_pos_local_left_LeftEye() const;
+    void set_current_joint_pos_local_left_LeftEye(const QVariantList &newCurrent_joint_pos_local_left_LeftEye);
+
+    int joint_pos_index_local_left_RightEye() const;
+    void set_joint_pos_index_local_left_RightEye(int newJoint_pos_index_local_left_RightEye);
+
+    int joint_pos_total_num_local_left_RightEye() const;
+    void set_joint_pos_total_num_local_left_RightEye(int newJoint_pos_total_num_local_left_RightEye);
+
+    QVariantList get_current_joint_pos_local_left_RightEye() const;
+    void set_current_joint_pos_local_left_RightEye(const QVariantList &newCurrent_joint_pos_local_left_RightEye);
+
+    int joint_pos_index_local_left_DuelEye() const;
+    void set_joint_pos_index_local_left_DuelEye(int newJoint_pos_index_local_left_DuelEye);
+
+    int joint_pos_total_num_local_left_DuelEye() const;
+    void set_joint_pos_total_num_local_left_DuelEye(int newJoint_pos_total_num_local_left_DuelEye);
+
+    QVariantList get_current_joint_pos_local_left_DuelEye() const;
+    void set_current_joint_pos_local_left_DuelEye(const QVariantList &newCurrent_joint_pos_local_left_DuelEye);
+
 signals:
     void joint_pos_index_following_right_Changed();
 
     void joint_pos_total_num_following_right_Changed();
 
     void current_joint_pos_following_right_changed();
-
-    void joint_pos_index_local_left_Changed();
-
-    void joint_pos_total_num_local_left_Changed();
-
-    void current_joint_pos_local_left_changed();
 
     void joint_pos_index_global_right_LeftEye_Changed();
 
@@ -132,13 +159,28 @@ signals:
 
     void current_joint_pos_global_right_DuelEye_changed();
 
+    void joint_pos_index_local_left_LeftEye_Changed();
+
+    void joint_pos_total_num_local_left_LeftEye_Changed();
+
+    void current_joint_pos_local_left_LeftEye_changed();
+
+    void joint_pos_index_local_left_RightEye_Changed();
+
+    void joint_pos_total_num_local_left_RightEye_Changed();
+
+    void current_joint_pos_local_left_RightEye_changed();
+
+    void joint_pos_index_local_left_DuelEye_Changed();
+
+    void joint_pos_total_num_local_left_DuelEye_Changed();
+
+    void current_joint_pos_local_left_DuelEye_changed();
+
 private:
     int m_joint_pos_index_following_right{0};
     int m_joint_pos_total_num_following_right{0};
     QVariantList m_current_joint_pos_following_right;
-    int m_joint_pos_index_local_left{0};
-    int m_joint_pos_total_num_local_left{0};
-    QVariantList m_current_joint_pos_local_left;
 
 private:
     bool copy_replace_file(QString &path_sourceFile, QString &path_targetFile);
@@ -150,7 +192,9 @@ private:
     std::vector<std::vector<double>> vector2d_jpos_list_global_right_RightEye_;
     std::vector<std::vector<double>> vector2d_jpos_list_global_right_DuelEye_;
     std::vector<std::vector<double>> vector2d_jpos_list_following_right_;
-    std::vector<std::vector<double>> vector2d_jpos_list_local_left_;
+    std::vector<std::vector<double>> vector2d_jpos_list_local_left_LeftEye_;
+    std::vector<std::vector<double>> vector2d_jpos_list_local_left_RightEye_;
+    std::vector<std::vector<double>> vector2d_jpos_list_local_left_DuelEye_;
     int m_joint_pos_index_global_right_LeftEye;
     int m_joint_pos_total_num_global_right_LeftEye;
     QVariantList m_current_joint_pos_global_right_LeftEye;
@@ -160,6 +204,15 @@ private:
     int m_joint_pos_index_global_right_DuelEye;
     int m_joint_pos_total_num_global_right_DuelEye;
     QVariantList m_current_joint_pos_global_right_DuelEye;
+    int m_joint_pos_index_local_left_LeftEye;
+    int m_joint_pos_total_num_local_left_LeftEye;
+    QVariantList m_current_joint_pos_local_left_LeftEye;
+    int m_joint_pos_index_local_left_RightEye;
+    int m_joint_pos_total_num_local_left_RightEye;
+    QVariantList m_current_joint_pos_local_left_RightEye;
+    int m_joint_pos_index_local_left_DuelEye;
+    int m_joint_pos_total_num_local_left_DuelEye;
+    QVariantList m_current_joint_pos_local_left_DuelEye;
 };
 
 #endif // CAMERACALIBQWRAPPER_H
